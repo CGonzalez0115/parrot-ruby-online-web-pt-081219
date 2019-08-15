8aaab968f2a9f603ffa050091a89bc80fb99fc7c
Christina Gonzalez <cmgamway@gmail.com>
2019-08-15 19:23:00 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -2,7 +2,7 @@ def parrot(phrase)
   :puts "Squawk!"
 end
 
-def return_parrot
+def return_parrot(phrase)
   :prints "Squawk!"
 end
 
@@ -10,6 +10,6 @@ def parrot(phrase)
   :puts "Pretty bird!"
 end
 
-def return_parrot
+def return_parrot(phrase)
   :prints "Pretty bird!"
 end
